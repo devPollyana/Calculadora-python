4e6ec3b7c758420f520233a49f4bdaf5f40d038d
Pollyana <pollyanarocha416@gmail.com>
2022-05-15 19:45:35 -0300

    Testando o quesito heranca

--- teste.py
+++ teste.py
@@ -12,4 +12,16 @@ class Funcionariosgeral(pessoasEmpresaX):
     def fazer_login(self):
         self.login = None
         pass
-
+# Pra nomear depois
+f1 = Funcionariosgeral()
+f1.nome = "Felipe"
+print(f"o nome do Funcionario: {f1.nome}")
+f1.salario = 1200
+print(f"O salario do Funcionario: {f1.salario}")
+#'''
+# f1 = Aluno()
+# f1.nome = "Pedro"
+# print(f"o nome do Aluno: {f1.nome}")
+# c1 = Aluno()
+# c1.cpf = "111.111.111-11"
+# print(f"O cpf do Aluno: {c1.cpf}\n") '''
