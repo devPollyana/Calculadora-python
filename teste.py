8161fa05e36f00ea63cf3fd40823f2d11aa8c994
Pollyana <pollyanarocha416@gmail.com>
2022-05-15 19:34:50 -0300

    class funcionario

--- teste.py
+++ teste.py
@@ -1,4 +1,15 @@
-class Meta:
-    def funcionario(self, name):
-        self.name = None
+class pessoasEmpresaX():  
+    def __init__(self):
+        self.cpf = None
+        self.nome = None
+        self.endereco = None
+
+class Funcionariosgeral(pessoasEmpresaX):
+
+    def __init__(self):
         self.salario = None
+    
+    def fazer_login(self):
+        self.login = None
+        pass
+
